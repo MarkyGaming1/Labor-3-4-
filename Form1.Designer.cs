1e2eaeecce9af60a467cf0dbd0340c99c9ee980e
MarkyGaming1 <92335136+MarkyGaming1@users.noreply.github.com>
2025-11-06 15:51:13 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Labor3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.back = new System.Windows.Forms.ToolStripButton();
+            this.foward = new System.Windows.Forms.ToolStripButton();
+            this.home = new System.Windows.Forms.ToolStripButton();
+            this.navigate = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.back,
+            this.foward,
+            this.home,
+            this.navigate,
+            this.toolStripTextBox1,
+            this.toolStripTextBox2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 31);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // back
+            // 
+            this.back.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.back.Image = ((System.Drawing.Image)(resources.GetObject("back.Image")));
+            this.back.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(29, 28);
+            this.back.Text = "toolStripButton1";
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // foward
+            // 
+            this.foward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.foward.Image = ((System.Drawing.Image)(resources.GetObject("foward.Image")));
+            this.foward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.foward.Name = "foward";
+            this.foward.Size = new System.Drawing.Size(29, 24);
+            this.foward.Text = "toolStripButton2";
+            this.foward.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // home
+            // 
+            this.home.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.home.Image = ((System.Drawing.Image)(resources.GetObject("home.Image")));
+            this.home.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.home.Name = "home";
+            this.home.Size = new System.Drawing.Size(29, 24);
+            this.home.Text = "toolStripButton3";
+            this.home.Click += new System.EventHandler(this.home_Click);
+            // 
+            // navigate
+            // 
+            this.navigate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.navigate.Image = ((System.Drawing.Image)(resources.GetObject("navigate.Image")));
+            this.navigate.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.navigate.Name = "navigate";
+            this.navigate.Size = new System.Drawing.Size(29, 24);
+            this.navigate.Text = "toolStripButton4";
+            this.navigate.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(250, 27);
+            this.toolStripTextBox1.Enter += new System.EventHandler(this.toolStripTextBox1_Enter);
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click);
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(294, 27);
+            this.toolStripTextBox2.Click += new System.EventHandler(this.toolStripTextBox2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(603, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(8, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 31);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(800, 419);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
+            this.webView21.Click += new System.EventHandler(this.webView21_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton back;
+        private System.Windows.Forms.ToolStripButton foward;
+        private System.Windows.Forms.ToolStripButton home;
+        private System.Windows.Forms.ToolStripButton navigate;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labor3
+{
+    public partial class Form1 : Form
+    {
+        private const string BLOCKED_WORDS_STRING = "porn,gambling,torrent";
+        private bool IsUrlbloced(string url)
+        {
+        string lowerUrl = url.ToLower();
+            string[] blockedWords = BLOCKED_WORDS_STRING.Split(',');
+            foreach (string word in blockedWords)
+            {
+                if (lowerUrl.Contains(word))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        string url;
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+            url = toolStripTextBox1.Text;
+            SetIE11Mode();
+            webView21.Source = new Uri("https://www.google.com");
+
+        }
+
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            url = toolStripTextBox1.Text;
+            if (IsUrlbloced(url))
+            {
+                MessageBox.Show("Access to this site is blocked.", "Blocked", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            webBrowser1.Navigate(url);
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                url = toolStripTextBox1.Text;
+                if (IsUrlbloced(url))
+                {
+                    MessageBox.Show("Access to this site is blocked.", "Blocked", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                webBrowser1.Navigate(url);
+            }
+
+        }
+
+        private void toolStripTextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.Equals(webBrowser1.Url))
+            {
+                toolStripTextBox2.Text = webBrowser1.Url.AbsoluteUri;
+            }
+        }
+
+        public static void SetIE11Mode()
+        {
+            string featureControlRegKey = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+            string appName = Path.GetFileName(AppDomain.CurrentDomain.FriendlyName);
+
+            try
+            {
+                using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(featureControlRegKey, true))
+                {
+                    if (registryKey != null)
+                    {
+                        registryKey.SetValue(appName, 11001, RegistryValueKind.DWord);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba a registry beállítása közben: " + ex.Message);
+            }
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
